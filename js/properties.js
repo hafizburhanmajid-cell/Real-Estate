b4ed2c752eadfdc8cf82d8812a6f5ea1a3acbcf2
hafizburhanmajid-cell <hafizburhanmajid@gmail.com>
2026-08-26 05:09:00 -0700

    changes

--- js/properties.js
+++ js/properties.js
@@ -8,6 +8,7 @@ const minPrice = document.getElementById("minPrice");
 const maxPrice = document.getElementById("maxPrice");
 const salerent = document.getElementById("sale-rent");
 const bedrooms=document.getElementById("bedrooms");
+const favbtn = document.querySelectorAll(".fav-btn");
 btn.addEventListener("click",function(){
     menu.classList.toggle("hidden");
 });
@@ -144,4 +145,26 @@ bedrooms.addEventListener("change",function(){
         const error = bedrooms.parentElement.querySelector(".err-pt");
         if(error) error.remove();
     }
+});
+resetFilter.addEventListener("click",function(){
+     propertytype.value = "";
+     location1.value = "";
+     minPrice.value = "";
+     maxPrice.value = "";
+     salerent.value = "";
+     bedrooms.value = "";
+     const propertyCards = document.querySelectorAll(".property-card");
+    propertyCards.forEach(function (card) {
+        card.classList.remove("hidden");
+    });
+});
+favbtn.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+        const icon = btn.querySelector("i");
+        if (icon) {
+            icon.classList.toggle("fa-regular");
+            icon.classList.toggle("fa-solid");
+            icon.classList.toggle("text-red-500");
+        }
+    });
 });
